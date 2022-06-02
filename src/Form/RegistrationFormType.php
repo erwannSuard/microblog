e7288d4265fbe64cd3a5aea7dd61db8d597bdf85
esuard <erwannsuardpro@gmail.com>
2022-06-02 16:56:49 +0200

    Account et obligation role_user sur la page

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -13,6 +13,7 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
+
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void

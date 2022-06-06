d77530e2add9fcef255db28559461cb357272d6b
esuard <erwannsuardpro@gmail.com>
2022-06-06 11:09:04 +0200

    page de chaque user

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    #[Route('/blog/{username}', name: 'blog')]
+    public function index(UserRepository $userRep, ArticleRepository $articleRep ,string $username): Response
     {
+        $user = $userRep->findOneBy(['username' => $username]);
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'user' => $user,
         ]);
     }
 }
